b2cadf45f67cabcca2b646bfe07b62e6d28fbb02
vigneshsv568 <vigneshsv568@gmail.com>
2022-07-25 17:32:57 +0530

    Adding forms, props & state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
-import React from 'react';
-
+import React,{useState} from 'react';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
      <h1>Vignesh Todo Application</h1>
      </header>
+     <Form
+      todos={todos}
+      inputText={inputText}
+       setTodos={setTodos} setInputText={setInputText}/>
+     <TodoList />
     </div>
   );
 }
